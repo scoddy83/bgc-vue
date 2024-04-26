ad51e89e322072ce44398455f5d13bd3490fc830
neil scott soutar <soutar.n@gmail.com>
2024-04-26 23:00:09 +0200

    v0.0.5

--- src/components/BannerPicture.vue
+++ src/components/BannerPicture.vue
@@ -1,4 +1,24 @@
 <script setup lang="ts">
+  import sanity from '../services/client'
+  import imageUrlBuilder from "@sanity/image-url";
+  const imageBuilder = imageUrlBuilder(sanity);
+
+  defineProps([
+    '_Page'
+  ])
+
+  const query = `*[_type == "banner" && page == _Page] {
+    page,
+    bild,
+    pageTitle,
+    shortText
+  }[0...10]`;
+
+  const banner = await sanity.fetch(query);
+
+  function imageUrlFor(source:string) : any {
+    return imageBuilder.image(source);
+  }
 </script>
 
 <template>
@@ -6,9 +26,8 @@
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Basler Garde Clique</h1>
-        <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
+        <h1 class="mb-5 text-5xl font-bold">{{ banner[0].pageTitle }}</h1>
+        <p class="mb-5">{{ banner[0].shortText }}</p>
       </div>
     </div>
   </div>
@@ -16,6 +35,6 @@
 
 <style scoped>
 .banner {
-  background-image: url(../assets/fasnacht2018.jpg);
+  background-image: url("imageUrlFor(bild)");
 }
 </style>
